9228eb7a112661a5aac8538cc8aed68b412ad70b
Luke Go <luke.go@hardkernel.com>
2016-12-14 11:52:07 +0900

    Revert "When Wi-Fi calling is available, be able to display only "Wi-Fi""
    This reverts commit 77a4ce6c6896b15ca32f8d1958f184e0c1eedddc.

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -633,17 +633,15 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                 && ((rule & SIMRecords.SPN_RULE_SHOW_SPN)
                         == SIMRecords.SPN_RULE_SHOW_SPN);
 
-        if (mPhone.getImsPhone() != null
+        if (!TextUtils.isEmpty(spn)
+                && mPhone.getImsPhone() != null
                 && ((ImsPhone) mPhone.getImsPhone()).isVowifiEnabled()) {
-            // In Wi-Fi Calling mode show SPN?+WiFi
+            // In Wi-Fi Calling mode show SPN+WiFi
             String formatVoice = mPhone.getContext().getText(
                     com.android.internal.R.string.wfcSpnFormat).toString();
             String formatData = mPhone.getContext().getText(
                     com.android.internal.R.string.wfcDataSpnFormat).toString();
-            String originalSpn = "";
-            if (!TextUtils.isEmpty(spn)) {
-                originalSpn = spn.trim();
-            }
+            String originalSpn = spn.trim();
             spn = String.format(formatVoice, originalSpn);
             dataSpn = String.format(formatData, originalSpn);
             showSpn = true;
